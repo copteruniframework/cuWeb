deeb0397a08343d224ebcd07fb9c70019e1b4ceb
maXe <max@copteruni.com>
2025-10-18 06:22:16 +0200

    feat(vite): auto build and minify: 'terser'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ export default defineConfig({
     root: '.', // Projektstamm
     build: {
         outDir: 'dist',
+        sourcemap: true,
+        minify: 'terser',
         rollupOptions: {
             input: './assets/js/main.js',
             output: {
